8f210e4a0b69f2bfe02b94cac292b5b7f17b5605
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-01-27 14:53:22 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
-
-
-
-// tailwind.config.js
-
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
+      screens: {
+        'sm': '320px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
       fontFamily: {
         primary: ['Inter', 'sans'],
       },
@@ -15,7 +16,6 @@ module.exports = {
         primary: {
           50: 'rgb(var(--tw-color-primary-50))',
           100: 'rgb(var(--tw-color-primary-100))',
-          // ... Define other color shades here
         },
         dark: '#222222',
       },
