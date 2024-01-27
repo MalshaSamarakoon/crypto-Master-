f2d6c68606294c5d299986ea8c3f9eab503d79f0
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-01-27 14:16:30 +0530

    Update route.js

--- src/app/api/courses/route.js
+++ src/app/api/courses/route.js
@@ -11,31 +11,56 @@ export async function POST(request) {
     price,
     duration,
     priceDescription,
+    image,
     courseContent,
     information,
   } = await request.json();
 
   await connectMongoDB();
 
+    // Extract the base64-encoded image data
+    const imageData = image && image.split(";base64,").pop();
+
+    // Build the image path
+    const imagePath = imageData ? `/courseUploads/${title}.png` : null;
+  
+    // Save the base64 image data to a file if available
+    if (imageData) {
+      const fs = require("fs");
+      const path = require("path");
+  
+      // Create the uploads directory if it doesn't exist
+      const uploadDir = path.join(process.cwd(), "public/courseUploads");
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+  
+      // Write the image file
+      const imageBuffer = Buffer.from(imageData, "base64");
+      const imageFilePath = path.join(uploadDir, `${title}.png`);
+      fs.writeFileSync(imageFilePath, imageBuffer);
+    }
+
   const courseContentArray = Array.isArray(courseContent)
     ? courseContent
     : [courseContent];
 
-    const informationArray = Array.isArray(information)
+  const informationArray = Array.isArray(information)
     ? information
     : [information];
 
-  await Course.create({
-    mode,
-    title,
-    description,
-    courseDescription,
-    price,
-    duration,
-    priceDescription,
-    courseContent: courseContentArray,
-    information: informationArray,
-  });
+    await Course.create({
+      mode,
+      title,
+      description,
+      courseDescription,
+      price,
+      duration,
+      priceDescription,
+      image: imagePath, 
+      courseContent: courseContentArray,
+      information: informationArray,
+    });
   return NextResponse.json({ message: "Course Created" }, { status: 201 });
 }
 
