9a58b72db1f28f7930668358fcdbb0fad8c89d76
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-01-27 14:11:25 +0530

    Update page.tsx

--- src/app/admin/addteam/page.tsx
+++ src/app/admin/addteam/page.tsx
@@ -5,20 +5,18 @@ import { useRouter } from "next/navigation";
 import useLoaded from "@/hooks/useLoaded";
 
 export default function AddTeam() {
-  // console.log("HI")
-
   const [name, setName] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [imageBase64, setImageBase64] = useState(""); // State to store base64 image
   const isLoaded = useLoaded();
-
   const router = useRouter();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     if (!name || !title || !description) {
-      alert("Name, Title and description are required.");
+      alert("Name, Title, and description are required.");
       return;
     }
 
@@ -28,11 +26,11 @@ export default function AddTeam() {
         headers: {
           "Content-type": "application/json",
         },
-        body: JSON.stringify({ name, title, description }),
+        body: JSON.stringify({ name, title, description, image: imageBase64 }),
       });
 
       if (res.ok) {
-        router.push("/");
+        router.push("/admin");
       } else {
         throw new Error("Failed to create a team");
       }
@@ -41,72 +39,70 @@ export default function AddTeam() {
     }
   };
 
+  const handleFileChange = async (e: any) => {
+    const selectedFile = e.target.files[0];
+    if (!selectedFile) return;
+
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const base64String = reader.result?.toString() || "";
+      setImageBase64(base64String);
+    };
+
+    reader.readAsDataURL(selectedFile);
+  };
+
   return (
     <>
-    <main style={{ backgroundColor: "white" }}>
-      <div className=" p-40 sm:ml-64 "> 
-        <section
-          className={clsx(
-            "min-h-main -mt-20 flex flex-col justify-center",
-            isLoaded && "fade-in-start"
-          )}
-        >
-          <section className="relative">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-
-              <label className="block text-sm font-medium text-dark">
-                Name
-              </label>
-              <input
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Name"
-              />
-              <label className="block text-sm font-medium text-dark">
-                Title
-              </label>
-              <input
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Title"
-              />
-
-              <label className="block text-sm font-medium text-dark">
-                Description
-              </label>
-              <input
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Member Description"
-              />
-
-              <div>
+      <main style={{ backgroundColor: "white" }}>
+        <div className="p-40 sm:ml-64">
+          <section
+            className={clsx(
+              "min-h-main -mt-20 flex flex-col justify-center",
+              isLoaded && "fade-in-start"
+            )}
+          >
+            <section className="relative">
+              <form onSubmit={handleSubmit} className="flex flex-col gap-3">
                 <label className="block text-sm font-medium text-dark">
-                  Image
+                  Name
                 </label>
-                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-500 border-solid rounded-md">
-                  <div className="space-y-1 text-center">
-                    <svg
-                      className="mx-auto h-12 w-12 text-dark"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                    <div className="flex text-sm text-gray-600">
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  className="border border-slate-500 px-8 py-2"
+                  type="text"
+                  placeholder="Name"
+                />
+                <label className="block text-sm font-medium text-dark">
+                  Title
+                </label>
+                <input
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                  className="border border-slate-500 px-8 py-2"
+                  type="text"
+                  placeholder="Title"
+                />
+
+                <label className="block text-sm font-medium text-dark">
+                  Description
+                </label>
+                <input
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                  className="border border-slate-500 px-8 py-2"
+                  type="text"
+                  placeholder="Member Description"
+                />
+
+                <div>
+                  <label className="block text-sm font-medium text-dark">
+                    Image
+                  </label>
+                  <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-500 border-solid rounded-md">
+                    <div className="space-y-1 text-center">
                       <label
                         htmlFor="file-upload"
                         className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
@@ -117,6 +113,7 @@ export default function AddTeam() {
                           name="file-upload"
                           type="file"
                           className="sr-only"
+                          onChange={handleFileChange}
                         />
                       </label>
 
@@ -125,21 +122,30 @@ export default function AddTeam() {
                     <p className="text-xs text-dark">
                       PNG, JPG, GIF up to 10MB
                     </p>
+                    {/* Display uploaded image */}
+                    {imageBase64 && (
+                      <div className="mt-3">
+                        <img
+                          src={imageBase64}
+                          alt="Uploaded Image"
+                          className="rounded-md max-w-full max-h-48 mx-auto"
+                        />
+                      </div>
+                    )}
                   </div>
                 </div>
-              </div>
 
-              <button
-                type="submit"
-                className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
-              >
-                Add Member
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+                >
+                  Add Member
+                </button>
+              </form>
+            </section>
           </section>
-        </section>
-      </div>
-    </main>
+        </div>
+      </main>
     </>
   );
 }
