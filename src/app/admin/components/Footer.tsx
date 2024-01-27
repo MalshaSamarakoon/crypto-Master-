7e61d0b39e02270ae3c407f02b5e73ba447e52c7
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-01-27 14:12:16 +0530

    Update Footer.tsx

--- src/app/admin/components/Footer.tsx
+++ src/app/admin/components/Footer.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 export default function AdFooter() {
   return (
-<footer className='z-50 relative'>
+<footer className='z-50 relative '>
       <div className='bg-neutral-900 p-6 text-center dark:bg-neutral-100'>
         <a
           className='font-semibold text-neutral-600 dark:text-neutral-400'
