336f36b3ad636085d9bd506a9691113f2c76130c
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-02-10 17:58:02 +0530

    Update Header.tsx

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 import clsx from 'clsx';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import Image from 'next/image';
 import Accent from '@/app/components/Accent';
