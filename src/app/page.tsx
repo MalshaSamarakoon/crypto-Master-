374d1159b80b90222478255c6d1ce4be71252b3c
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-03-20 22:34:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function HomePage() {
             <div className="a2a_kit a2a_kit_size_32 a2a_default_style fixed bottom-12 right-12 z-10 w-16 h-16">
               {/* WhatsApp chat button */}
               <a
-                href="https://api.whatsapp.com/send?phone=947671131"
+                href="https://api.whatsapp.com/send?phone=94713426510"
                 target="_blank"
                 rel="noopener noreferrer"
               >
