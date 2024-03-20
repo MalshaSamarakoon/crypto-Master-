ebf1b6e12ae8ab4560bf76f42178fbc53ff27b4d
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-03-20 23:09:55 +0530

    Update page.tsx

--- src/app/pages/services/ServiceDetails/Cryptocurrency/Details/page.tsx
+++ src/app/pages/services/ServiceDetails/Cryptocurrency/Details/page.tsx
@@ -1,192 +1,213 @@
 "use client";
+import Head from "next/head";
 
 export default function Details() {
   return (
-    <main>
-      <section className=" layout min-h-main flex flex-col justify-center sm:px-6 lg:px-0">
-        <div className="lg:mt-20 sm:mt-10 flex min-h-screen flex-col py-1 mb-20">
-          <h1 className="mb-20 text-white  md:text-5xl ">
-            Moon Coin Subscription
-          </h1>
-          <div className=" mb-20 grid lg:grid-cols-3 lg:gap-40 sm:grid-cols-1">
-            <div className=" col-span-2 lg:mt-8 sm:mt-0 leading-relaxed ">
-              <p className="text-gray-200 opacity-80 md:text-lg text-xl ">
-                {" "}
-                Embark on a journey to elevate your crypto trading experience
-                with Moon Coin Subscription , where precision meets
-                profitability. Our group is dedicated to providing high-quality
-                signals, empowering you to navigate the dynamic cryptocurrency
-                market with confidence.
-              </p>
-            </div>
-            <div className="col-span-1 p-10 sm:mt-10 lg:mt-0">
-              <div className=" text-gray-200"></div>
-              <div className="">
-                <button className="h-12 sm:w-60 lg:w-80 rounded-md bg-amber-500 text-lg font-semibold text-white transition duration-300 ease-in-out hover:bg-amber-600">
-                  ENROLL
-                </button>
-              </div>
-            </div>
-          </div>
-          <h1 className="text-white lg:mt-6 text-center md:text-2xl ">
-            You can choose the most convenient package for your self..{" "}
-          </h1>
-          <img
-            src="/images/McsPackages.png"
-            alt="Your Alt Text"
-            className="w-full py-20  lg:px-60 object-cover"
-          />
-          <div className="text-white">
-            <ul>
-              <li className="mb-8 text-lg flex items-center">
-                <span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                  >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Expert Signals: Our team of experienced analysts provides
-                accurate and timely signals for various cryptocurrencies.
-              </li>
-
-              <li className="mb-8 text-lg flex items-center">
-                <span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                  >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Market Insights: Stay updated with the latest market trends,
-                news, and insights to make informed trading decisions.
-              </li>
-              <li className="mb-8 text-lg flex items-center">
-                <span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                  >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Community Support: Connect with like-minded traders, share
-                knowledge, and grow together in a supportive community.
-              </li>
-            </ul>
-            <h1 className="text-white mt-10 mb-10 md:text-2xl ">
-              What to Expect..{" "}
+    <div>
+      <Head>
+        {/* Add the WhatsApp chat widget script */}
+        <script async src="https://static.addtoany.com/menu/page.js"></script>
+      </Head>
+      <main>
+        <section className=" layout min-h-main flex flex-col justify-center sm:px-6 lg:px-0">
+          <div className="lg:mt-20 sm:mt-10 flex min-h-screen flex-col py-1 mb-20">
+            <h1 className="mb-20 sm:mb-10 text-white  md:text-5xl ">
+              Moon Coin Subscription
             </h1>
-            <ul>
-              <li className="mb-8 text-lg flex items-center"><span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
+            <div className=" mb-20 grid lg:grid-cols-3 lg:gap-40 sm:grid-cols-1">
+              <div className=" col-span-2 lg:mt-8 sm:mt-0 leading-relaxed ">
+                <p className="text-gray-200 opacity-80 md:text-lg text-xl sm:text-sm ">
+                  {" "}
+                  Embark on a journey to elevate your crypto trading experience
+                  with Moon Coin Subscription , where precision meets
+                  profitability. Our group is dedicated to providing
+                  high-quality signals, empowering you to navigate the dynamic
+                  cryptocurrency market with confidence.
+                </p>
+              </div>
+              <div className="col-span-1 p-10 sm:mt-10 lg:mt-0 sm:mb-0">
+  <div className="text-gray-200"></div>
+  <div className="">
+    <a
+      href="https://api.whatsapp.com/send?phone=94713426510"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {/* Apply shake animation continuously */}
+      <button 
+        className="h-12 sm:w-60 lg:w-80 rounded-md bg-amber-500 text-lg font-semibold text-white transition duration-300 ease-in-out hover:bg-amber-600 shake-on-hover"
+      >
+        ENROLL
+      </button>
+    </a>
+  </div>
+</div>
+
+            </div>
+            <h1 className="text-white lg:mt-6 sm:mt-0 text-center md:text-2xl ">
+              You can choose the most convenient package for your self..{" "}
+            </h1>
+            <img
+              src="/images/McsPackages.png"
+              alt="Your Alt Text"
+              className="w-full py-20  lg:px-60 object-cover"
+            />
+            <div className="text-white">
+              <ul>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
                   >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Daily signals featuring clear entry and exit points.
-              </li>
-              <li className="mb-8 text-lg flex items-center"><span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Expert Signals: Our team of experienced analysts provides
+                  accurate and timely signals for various cryptocurrencies.
+                </li>
+
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
                   >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                In-depth analysis and comprehensive market updates.
-              </li>
-              <li className="mb-8 text-lg flex items-center"><span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Market Insights: Stay updated with the latest market trends,
+                  news, and insights to make informed trading decisions.
+                </li>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
                   >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Educational content designed to enhance your trading skills.
-              </li>
-              <li className="mb-8 text-lg flex items-center"><span
-                  className="mr-2 text-amber-500"
-                  style={{ marginRight: "0.5rem" }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-check-circle"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Community Support: Connect with like-minded traders, share
+                  knowledge, and grow together in a supportive community.
+                </li>
+              </ul>
+              <h1 className="text-white mt-10 mb-10 md:text-2xl ">
+                What to Expect..{" "}
+              </h1>
+              <ul>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
                   >
-                    <path d="M0 0h16v16H0z" fill="none" />
-                    <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
-                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
-                  </svg>
-                </span>
-                Engaging community discussions fostering growth and learning.
-              </li>
-            </ul>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Daily signals featuring clear entry and exit points.
+                </li>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  In-depth analysis and comprehensive market updates.
+                </li>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Educational content designed to enhance your trading skills.
+                </li>
+                <li className="mb-8 text-lg flex items-center">
+                  <span
+                    className="mr-2 text-amber-500"
+                    style={{ marginRight: "0.5rem" }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="bi bi-check-circle"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                    >
+                      <path d="M0 0h16v16H0z" fill="none" />
+                      <path d="M8 0a.5.5 0 0 1 .5.5V14a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l6-6z" />
+                    </svg>
+                  </span>
+                  Engaging community discussions fostering growth and learning.
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </div>
   );
 }
