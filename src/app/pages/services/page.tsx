5448eaf58b9023b6f54e1c9603befa07a7244959
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-03-20 22:22:15 +0530

    Update page.tsx

--- src/app/pages/services/page.tsx
+++ src/app/pages/services/page.tsx
@@ -8,11 +8,12 @@ import Academic from "./ServiceDetails/Academic/page";
 
 export default function ServicesPage() {
   const services = [
-    { name: "Cryptocurrency", component: <Cryptocurrency /> },
-    { name: "Forex Trading", component: <Forex /> },
+    { name: "Crypto", component: <Cryptocurrency /> },
+    { name: "Forex", component: <Forex /> },
     { name: "ICT", component: <IT /> },
-    { name: "Academic", component: <Academic /> },
+    { name: "Other", component: <Academic /> },
   ];
+
   interface Service {
     name: string;
     component: React.ReactNode;
@@ -29,7 +30,7 @@ export default function ServicesPage() {
   return (
     <main>
       <Head>
-        <title>Hi</title>
+        <title>Moon Education | Services</title>
       </Head>
 
       <section className=" relative">
@@ -41,64 +42,63 @@ export default function ServicesPage() {
             >
               Our Services
             </h1>
-          </div>
-          <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-40 lg:mt-20 sm:mt-2 sm:px-6 lg:px-20">
-            <div className="lg:col-span-1 sm:col-span-1">
-              {services.map((service, index) => (
+
+
+          <div className="grid sm:grid-cols-4 lg:gap-10 sm:mt-2  mb-20 lg:mt-10 sm:mt-0">
+            {services.map((service, index) => (
+              <div
+                key={index}
+                className="cursor-pointer sm:h-10 lg:h-20 lg:text-2xl sm:text-sm my-20 sm:my-8 "
+                style={{
+                  textDecoration:
+                    selectedService === service ? "underline" : "none",
+                  transition: "color 0.3s, text-decoration 0.3s",
+                }}
+                onClick={() => handleServiceClick(service)}
+              >
                 <div
-                  key={index}
-                  className="cursor-pointer sm:h-20 lg:h-20 lg:text-2xl sm:text-xl"
-                  style={{
-                    textDecoration:
-                      selectedService === service ? "underline" : "none",
-                    transition: "color 0.3s, text-decoration 0.3s",
-                  }}
-                  onClick={() => handleServiceClick(service)}
+                  className={`bg-neutral-900 lg:py-8 sm:py-4 lg:my-6  text-amber-400 flex justify-center `}
                 >
-                  <div
-                    className={`bg-neutral-900 py-8 my-6 pl-10 text-amber-400`}
-                  >
-                    {service.name}
-                  </div>
+                  {service.name}
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
+          </div>
+          <div className="lg:col-span-2 sm:col-span-1 lg:mt-4 sm:-mt-20 sm:mb-20 text-neutral-300 ">
+            {selectedService ? (
+              selectedService.component
+            ) : (
+              <>
+                <p className="my-4 max-w-4xl text-left text-neutral-400 md:text-lg 2xl:text-lg">
+                  At Moon Education Center our team of dedicated educators and
+                  experts is here to guide you on your way to success. We are
+                  committed to making knowledge accessible to all, offering a
+                  wide range of resources and training to help you harness the
+                  potential of new technologies and industries.
+                </p>
 
-            <div className="lg:col-span-2 sm:col-span-1 lg:mt-4 sm:-mt-20 sm:mb-20 text-neutral-300">
-              {selectedService ? (
-                selectedService.component
-              ) : (
-                <>
-                  <p className="my-4 max-w-4xl text-left text-white md:text-lg 2xl:text-xl">
-                    At Moon Education Center our team of dedicated educators and
-                    experts is here to guide you on your way to success. We are
-                    committed to making knowledge accessible to all, offering a
-                    wide range of resources and training to help you harness the
-                    potential of new technologies and industries.
-                  </p>
+                <br />
+                <p className="my-4 max-w-4xl text-left text-neutral-400 md:text-lg 2xl:text-lg">
+                  Embark on a transformative learning journey with Moon
+                  Education Center&apos;s diverse range of services. Our
+                  commitment extends beyond imparting knowledge; we foster an
+                  inclusive and dynamic learning environment where you can
+                  engage with cutting-edge concepts, connect with industry
+                  experts, and cultivate the skills needed to thrive in
+                  today&apos;s rapidly evolving landscape. Join us in shaping
+                  a future where education paves the way to unparalleled
+                  financial success and personal growth.
+                </p>
 
-                  <br />
-                  <p className="my-4 max-w-4xl text-left text-white md:text-lg 2xl:text-xl">
-                    Embark on a transformative learning journey with Moon
-                    Education Center&apos;s diverse range of services. Our
-                    commitment extends beyond imparting knowledge; we foster an
-                    inclusive and dynamic learning environment where you can
-                    engage with cutting-edge concepts, connect with industry
-                    experts, and cultivate the skills needed to thrive in
-                    today&apos;s rapidly evolving landscape. Join us in shaping
-                    a future where education paves the way to unparalleled
-                    financial success and personal growth.
-                  </p>
+                <br />
 
-                  <br />
-
-                  <p className="my-4 max-w-4xl text-left text-white md:text-lg 2xl:text-xl">
-                    You can now explore our services to turn your dreams into
-                    reality.
-                  </p>
-                </>
-              )}
-            </div>
+                <p className="my-4 max-w-4xl text-left text-neutral-400 md:text-lg 2xl:text-lg">
+                  You can now explore our services to turn your dreams into
+                  reality.
+                </p>
+              </>
+            )}
+          </div>
           </div>
         </div>
       </section>
