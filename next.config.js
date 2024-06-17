d2816ace1ebbb3d6b2733d53b13450c9554b19d2
Malsha Samarakoon <chamodi96samarakoon@gmail.com>
2024-06-17 16:08:12 +0530

    Fix sitemap and optimize images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    // Other Next.js configurations
+  };
+  
+  async function getConfig() {
+    const { default: sitemapConfig } = await import('./next-sitemap.js');
+    return {
+      ...nextConfig,
+      sitemap: sitemapConfig,
+    };
+  }
+  
+  module.exports = getConfig();
+  
