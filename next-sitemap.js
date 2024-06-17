7210ecc30801aad2b0fd4cf68d9fd9173defb83e
Malsha Samarakoon <chamodi96samarakoon@gmail.com>
2024-06-17 15:12:26 +0530

    Add sitemap configuration

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mooneducationcenter.com', 
+    generateRobotsTxt: true, // (optional)
+    // Additional options if needed
+  };
+  
