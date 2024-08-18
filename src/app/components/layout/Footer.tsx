f28d319cbce63ab2803ef1defcfb2d0d119ca881
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:39:15 +0530

    Update Footer.tsx

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,10 +1,10 @@
 "use client";
-
+import Image from "next/image";
 import * as React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-30 border-t  border-neutral-500 pb-2 pt-20">
+    <footer style={{ backgroundColor: '#002147' }} className=" border-t  border-neutral-500 pb-2 pt-20 ">
       <main className="layout flex flex-col items-center">
         <footer className=" text-center text-neutral-500  lg:text-left">
           <div className="flex items-center justify-center ">
@@ -84,6 +84,10 @@ export default function Footer() {
           <div className="mx-6 py-10 text-center md:text-left">
             <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
               <div className="">
+              <div className="flex justify-center md:justify-start items-center mb-10">
+            {/* Logo container */}
+            <Image src="/images/Logo.PNG" alt="Logo" width={100} height={80} />
+          </div>
                 <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
                   Moon Education
                 </h6>
@@ -109,7 +113,7 @@ export default function Footer() {
                     href="#!"
                     className="text-neutral-600 dark:text-neutral-200"
                   >
-                    Forex
+                    Forex Trading
                   </a>
                 </p>
                 <p className="mb-4">
@@ -180,12 +184,7 @@ export default function Footer() {
                     <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
                     <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
                   </svg>
-                  <a
-                    href="https://maps.app.goo.gl/p72ypZuKYAUru46i6"
-                    className="mr-6 text-neutral-600 dark:text-neutral-200"
-                  >
-                    Bodhiraja Mawatha, Kurunegala{" "}
-                  </a>
+                  Bodhiraja Mawatha, Kurunegala
                 </p>
                 <p className="mb-4 flex items-center justify-center md:justify-start">
                   <svg
@@ -197,12 +196,7 @@ export default function Footer() {
                     <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                     <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                   </svg>
-                  <a
-                    href="#"
-                    className="mr-6 text-neutral-600 dark:text-neutral-200"
-                  >
                   mooneducationcenter@gmail.com
-                  </a>
                 </p>
                 <p className="mb-4 flex items-center justify-center md:justify-start">
                   <svg
@@ -231,7 +225,7 @@ export default function Footer() {
           </div>
         </footer>
       </main>
-      <div className="bg-neutral-900 p-6 text-center dark:bg-neutral-700">
+      <div style={{ backgroundColor: '#01162d' }} className=" p-6 text-center ">
         {/* <span>© 2023 Copyright:</span> */}
         <a
           className="font-semibold text-neutral-600 dark:text-neutral-400"
