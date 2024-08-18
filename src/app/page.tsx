0dcadff760e6a1eb7c6a06167de31c41c1ef5cd1
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:11:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import ChooseLanding from "./components/ChooseLanding";
 import FeedbacksLanding from "./components/FeedbacksLanding";
 import Seperator from "./components/Seperator";
 import LatestLanding from "./components/LatestLanding";
-import Head from "next/head";
+import YoutubeVideo from "./components/YoutubeVideo";
 
 export default function HomePage() {
   const [scrollBg, setScrollBg] = React.useState(false);
@@ -30,121 +30,123 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div>
-      <Head>
-        {/* Add the WhatsApp chat widget script */}
-        <script async src="https://static.addtoany.com/menu/page.js"></script>
-        <meta name="google-site-verification" content="fcbRWJaorkbcj6GGJZ-p3WObHovaOLwxA6V20c-RrEs" />
-      </Head>
-      <main className="w-screen ">
-        <Header />
-        <section className="">
-          <div className="fixed top-0 left-0 w-screen h-screen z-0">
-            <img
-              src="/images/test.png"
-              alt="Your Alt Text"
-              className="w-full h-full object-cover"
-            />
-          </div>
+    <main>
+      <Header />
+      <section className="relative">
+        <div
+          className="relative content-container z-10 text-lg md:text-xl flex items-center justify-center"
+          style={{ position: "relative", width: "100%" }}
+        >
+          <img
+            src="/images/zz.jpg"
+            alt="Hero Image"
+            className="w-full h-full object-cover col-span-2"
+          />
+          <div className="absolute inset-0 grid grid-cols-2">
+            <div className="flex flex-col items-center justify-center text-center z-20 ">
+              <h1
+                className="text-2xl sm:text-sm md:text-6xl md:ml-10 uppercase mb-2"
+                style={{
+                  color: "#00214790",
+                  lineHeight: "1.3",
+                  fontWeight: 800,
+                }}
+              >
+                Empowering Minds for a Brighter Future
+              </h1>
 
-          <section className=" relative">
-            <div className="flex justify-center w-screen h-screen md:mt-40 sm:mt-20 ">
-              <div className="layout content-container sm:mx-10">
-                <h1
-                  className="text-neutral-100 text-left md:text-7xl md:leading-snug "
-                  data-fade="2"
+              <h6
+                className="hidden lg:block text-2xs sm:text-2xs md:text-xl mb-8"
+                style={{ color: "#00214790" }}
+              >
+                We empower individuals with knowledge and tech solutions to
+                navigate tomorrow's world.
+              </h6>
+
+              <div className="flex justify-center space-x-4">
+                <ButtonLink
+                  href="/courses"
+                  className="btn-primary hover:bg-[#8d6e1b] transition duration-300"
+                  style={{ backgroundColor: "#a67d21", border: "none" }}
                 >
-                  Turning Knowledge into Wealth.. <br /> Your Success.. <br />{" "}
-                  Our Expertise !
-                </h1>
-                <p
-                  className={clsx(
-                    "mt-4 max-w-4xl text-left text-neutral-400 md:mt-10",
-                    "md:text-xl 2x1:text-2xl"
-                  )}
-                  data-fade="3"
-                >
-                  Empowering individuals with knowledge and tech solutions to
-                  navigate tomorrow world.
-                </p>
+                  Go to Courses
+                </ButtonLink>
               </div>
             </div>
+            <div className="hidden md:block"></div>
+          </div>
+        </div>
 
-            <div className=" relative content-container bg-dark z-20">
-              <Praise />
-            </div>
+        <Praise />
 
-            <div className="relative content-container bg-dark z-10 ">
-              <article
-                className={clsx(
-                  "layout flex flex-col items-center md:flex-row md:justify-start",
-                  "md:gap-4 lg:pt-40 sm:pt-20 sm:pb-20 "
-                )}
-                data-fade="0"
-              >
-                <div className="relative h-full w-full ">
-                  <h2 className="text-4xl md:text-6xl sm:mx-10 lg:mx-0">
-                    <Accent className="inline decoration-clone leading-snug dark:leading-none">
-                      Elevate Your Journey with Our Range of Services{" "}
-                    </Accent>
-                  </h2>
+        <div className="relative content-container bg-dark z-10 flex flex-col items-center justify-center text-center">
+          <section className="relative">
+            <div className="h-full w-full layout items-center px-0 py-8 md:py-16">
+              <div className="relative">
+                <h1
+                  className="text-xl md:text-3xl sm:mx-10 lg:mx-0 md:leading-relaxed tracking-wide"
+                  style={{ color: "#01162d" }}
+                >
+                  Start Your Journey with Our
+                  <br />
+                  Range of Services
+                </h1>
+              </div>
+
+              <div className="flex flex-col items-center justify-center mt-2 md:mt-10">
+                <div
+                  className="text-base text-neutral-800 text-sm md:text-m sm:mx-10 lg:mx-0"
+                  style={{ lineHeight: "2", letterSpacing: "0px" }}
+                >
+                  At Moon Education Center, we lead in educational excellence
+                  with our innovative and comprehensive approach. We offer
+                  cutting-edge Vocational Training Programs and School
+                  Curriculum Support Classes in a state-of-the-art computer lab
+                  environment.
                 </div>
-                <div className="h-full w-full">
-                  <ul className="relative h-full">
-                    <div
-                      className="mt-10 text-base text-neutral-300 md:text-xl sm:mx-10 lg:mx-0"
-                      style={{ lineHeight: "2", letterSpacing: "0.9px" }}
-                    >
-                      At <span>Moon Education Center</span>, our team of
-                      dedicated educators and experts is here to guide you on
-                      your path to financial success. We are committed to making
-                      knowledge accessible to all, offering a wide range of
-                      resources and training to help you harness the potential
-                      of new technologies and industries. Explore our services
-                      to turn your dreams into reality.
-                    </div>
-                  </ul>
-                </div>
-              </article>
-            </div>
 
-            <div className=" relative content-container bg-dark lg:pb-20 ">
-              <ServicesLanding />
-            </div>
-            <div className=" relative content-container bg-dark ">
-              <Seperator />
-            </div>
-            <div className=" relative content-container bg-dark lg:pb-20 sm:pb-20">
-              <ChooseLanding />
-            </div>
-            <div className=" relative content-container bg-dark ">
-              <LatestLanding />
-            </div>
-            <div className=" relative content-container bg-dark ">
-              <Seperator />
-            </div>
-            <div className=" relative content-container bg-dark ">
-              <FeedbacksLanding />
-            </div>
-            <div className=" relative content-container bg-dark ">
-              <Seperator />
-            </div>
-            <div className="a2a_kit a2a_kit_size_32 a2a_default_style fixed bottom-12 right-12 z-20 w-16 h-16">
-              {/* WhatsApp chat button */}
-              <a
-                href="https://api.whatsapp.com/send?phone=94713426510"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/whatsapp.png" alt="WhatsApp Chat" />
-              </a>
-            </div>
-            <div className=" relative content-container bg-dark">
-              <Footer />
+                <a
+                  href="/pages/about"
+                  className="mt-4 md:mt-10 px-6 py-2 text-white bg-[#a67d21] hover:bg-[#8d6e1b] transition duration-300 inline-block border border-round-lg rounded-md"
+                >
+                  Read More &gt;
+                </a>
+              </div>
             </div>
           </section>
-        </section>
-      </main>
-    </div>
+        </div>
+
+        <div className="relative content-container bg-dark">
+          <ServicesLanding />
+        </div>
+        <div className="relative content-container bg-dark">
+          <Seperator />
+        </div>
+        {/* <div className="relative content-container bg-dark">
+          <FeedbacksLanding />
+        </div> */}
+        <div className="relative content-container bg-dark">
+          <ChooseLanding />
+        </div>
+        <div
+          className="relative content-container py-20"
+          style={{ backgroundColor: "#a67d2150" }}
+        >
+          <LatestLanding />
+        </div>
+        <div className="relative content-container ">
+          <Seperator />
+        </div>
+        <div className="relative content-container bg-dark ">
+          <YoutubeVideo
+            title="Your Video Title"
+            url="https://www.youtube.com/watch?v=q7MSBZpYw-Y"
+          />
+        </div>
+        <div className="relative content-container bg-dark">
+          <Footer />
+        </div>
+      </section>
+    </main>
   );
 }
