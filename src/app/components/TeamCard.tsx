c9a3aa4348bbe8adb5c52623cdce8e29efebb25d
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:40:32 +0530

    Update TeamCard.tsx

--- src/app/components/TeamCard.tsx
+++ src/app/components/TeamCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const getTeams = async () => {
+const getteams = async () => {
   try {
-    const res = await fetch(`${process.env.BASE_URL}/api/teams`, {
+    const res = await fetch("http://localhost:3000/api/teams", {
       cache: "no-store",
     });
 
@@ -13,29 +13,29 @@ const getTeams = async () => {
     return res.json();
   } catch (error) {
     console.log("Error loading teams: ", error);
-    return { teams: [] }; // Return an empty array in case of error
   }
 };
 
 export default async function TeamCard() {
-  const { teams } = await getTeams();
+  const { teams } = (await getteams()) || {};
 
   return (
     <>
-      {teams?.map((m: any) => (
-        <div key={m._id}>
-          <div className="bg-bg-dark rounded-[15px] shadow-xl border-[2px] border-[#d9b157] text-[#E7F1FB] relative w-[250px] max-w-full text-center flex flex-col items-center pt-8 pb-8">
-            <div className="-mt-20">
+      {teams.map((m: any) => (
+        <div key={m._id} className="w-[250px] max-w-full text-center flex flex-col items-center  pt-8 pb-8">
+          <div className="bg-bg-dark border-[2px] border-[#d9b157] shadow-xl text-[#002147] relative w-full p-4">
+            <div className="relative w-full h-[250px] mb-4 overflow-hidden flex items-center justify-center">
               <img
-                className="border-[2px] border-[#d9b157] rounded-full w-[100px] h-[100px] object-cover p-1"
-                src={m.image} 
+                className="w-full h-full object-cover"
+                src={m.image}
                 alt={m.name}
               />
             </div>
-            <h4 className="mt-5 mb-5 text-[#E7F1FB] relative w-[250px] ">
+
+            <h6 className="mt-1 mb-1 uppercase p-1 text-[#002147]">{m.title}</h6>
+            <h4 className="mt-2 mb-0 text-[#002147]">
               {m.name}
             </h4>
-            <h3 className="mt-1 mb-1 uppercase p-1 text-gold ">{m.title}</h3>
             <p className="text-sm leading-[21px] p-4">{m.description}</p>
           </div>
         </div>
