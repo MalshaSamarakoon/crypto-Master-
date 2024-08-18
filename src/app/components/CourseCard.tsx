e968d9c9f70cd0061ce3d2bfa4e37bdd0866b5ca
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:51:57 +0530

    Update CourseCard.tsx

--- src/app/components/CourseCard.tsx
+++ src/app/components/CourseCard.tsx
@@ -2,53 +2,32 @@ import React from "react";
 import Button from "@/components/buttons/Button";
 import Link from "next/link";
 
-const getcourses = async () => {
-  try {
-    const res = await fetch(`${process.env.BASE_URL}/api/courses`, {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch courses");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log("Error loading courses: ", error);
-    return { courses: [] }; // Return an empty array in case of error
-  }
-};
-
-export default async function CourseCard() {
-  const { courses } = await getcourses();
-
+export default function CourseCard({ course }) {
   return (
-    <>
-      {courses.map((m: any) => (
-        <div key={m._id}>
-          <div className="bg-bg-dark relative items-center rounded-[15px] border-[2px] border-[#d9b157] pb-8 text-center text-[#E7F1FB] shadow-xl">
-            <img
-              className="h-[150px] w-full rounded-t-[15px] object-cover object-top"
-              src={m.image} 
-              alt={m.title}
-              />
-            <div className="absolute top-0 left-0 bg-red-500 rounded-[8px] text-white text-s p-1 m-4 rounded-bl-md">
-              {m.mode}
-            </div>
-            <h3 className="relative mb-5 mt-5 text-[#E7F1FB]">{m.title}</h3>
-            <p className="p-4 text-sm leading-[21px] mb-10">{m.description}</p>
-
-            <Link href={`/pages/courses/CourseDetails?_id=${m._id}`}>
-              <Button
-                className="absolute bottom-4 right-4 border border-amber-400 text-amber-400 hover:bg-neutral-700 dark:hover:bg-neutral-700"
-                variant="outline"
-              >
-                More Details
-              </Button>
-            </Link>
-          </div>
+    <div>
+      <div className="bg-bg-dark relative items-center  border-[2px] border-[#d9b157] pb-8 text-center text-[#002147] shadow-xl">
+        {course.image && (
+          <img
+            className="h-[150px] w-full object-cover object-top"
+            src={course.image}
+            alt={course.title}
+          />
+        )}
+        <div className="absolute top-0 left-0 bg-red-500 rounded-[8px] text-[#002147] text-s p-1 m-4 rounded-bl-md">
+          {course.mode}
         </div>
-      ))}
-    </>
+        <h3 className="relative mb-5 mt-5 text-[#002147]">{course.title}</h3>
+        <p className="p-4 text-[#002147] text-sm leading-[21px] mb-0">{course.description}</p>
+
+        {/* <Link href={`/pages/courses/CourseDetails?_id=${course._id}`}>
+          <Button
+            className="absolute bottom-4 right-4 border border-amber-400 text-amber-400 hover:bg-neutral-700 dark:hover:bg-neutral-700"
+            variant="outline"
+          >
+            More Details
+          </Button>
+        </Link> */}
+      </div>
+    </div>
   );
 }
