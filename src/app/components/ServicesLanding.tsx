52c1176a3453aa0ae8cc7e2c881d52c3eeda2db5
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:41:17 +0530

    Update ServicesLanding.tsx

--- src/app/components/ServicesLanding.tsx
+++ src/app/components/ServicesLanding.tsx
@@ -1,53 +1,91 @@
-// ServicesLanding.tsx
 import React from "react";
 import "./FeedbacksLanding.css";
+import Image from "next/image";
 
 export default function ServicesLanding() {
-  const services = [
-    { name: "Forex" },
-    { name: "Cryptocurrency" },
-    { name: "ICT" },
-    { name: "Academic" },
-  ];
-
   return (
-    <section className="relative">
-      <div
-        className="relative content-container z-10 text-lg md:text-xl"
-        style={{
-          backgroundImage: "url('/images/service3.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          position: "relative",
-          overflow: "hidden",
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "60%",
-            background: "linear-gradient(to bottom, #222222, rgba(0,0,0,0))",
-          }}
-        />
+    <section className="relative mx-10 md:mx-20">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        {/* First Column */}
+        <div className="col-span-1">
+          {/* Long grid */}
+          <div className="h-96 mb-4 relative">
+            <Image
+              src="/images/grid2.JPG"
+              alt="grid"
+              layout="fill"
+              objectFit="cover"
+              className="absolute inset-0"
+            />
+          </div>
 
-        <div className="lg:h-60 sm:h-40"></div>
-        <div className="lg:h-80 sm:h-40"></div>
+          {/* Small grid */}
+          <div
+            style={{ backgroundColor: "#1557a2" }}
+            className="h-64 bg-gray-300 p-6 text-white"
+          >
+            <h2 className="text-xl font-bold mb-4">Educational Support Programs</h2>
+            <p className="text-sm">
+              Boost your academic performance with our Educational Support Programs, offering practical resources and guidance from highly qualified staff.
+            </p>
+          </div>
+        </div>
 
-        <div className="flex items-center justify-center w-4/5 absolute left-1/2 transform -translate-x-1/2 -translate-y-full  lg:mt-0 lg:-translate-y-3/4">
-          <div className="flex-1 bg-dark relative sm:opacity-70 lg:opacity-100">
-            {/* <div className="border-dashed border-2 border-teal-500 border-top"></div> */}
-            <div className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-4 lg:gap-20 ">
-              {services.map((service, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center box-container relative sm:h-20 lg:h-40 text-teal-500"
-                >
-                  <p style={{ color: "#39FFFF" }}>{service.name}</p>
-                </div>
-              ))}
+        {/* Second Column */}
+        <div className="col-span-1">
+          {/* Small grid */}
+          <div
+            style={{ backgroundColor: "#773928" }}
+            className="h-64 bg-gray-300 mb-4 p-6 text-white"
+          >
+            <h2 className="text-xl font-bold mb-4">Vocational Training Programs</h2>
+            <p className="text-sm">
+              Enhance your career prospects with our Vocational Training Programs, featuring state-of-the-art facilities and expert instructors.
+            </p>
+          </div>
+          {/* Long grid */}
+          <div className="h-96 relative">
+            <Image
+              src="/images/grid1.JPG"
+              alt="grid"
+              layout="fill"
+              objectFit="cover"
+              className="absolute inset-0"
+            />
+          </div>
+        </div>
+
+        {/* Third and Fourth Columns */}
+        <div className="col-span-1 md:col-span-2 grid grid-rows-2">
+          {/* Long grid */}
+          <div className="row-span-2 h-96 mb-4 relative">
+            <Image
+              src="/images/grid3.JPG"
+              alt="grid"
+              layout="fill"
+              objectFit="cover"
+              className="absolute inset-0"
+            />
+          </div>
+          {/* Additional grids */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div
+              style={{ backgroundColor: "#15a295" }}
+              className="h-64 bg-gray-300 p-6 text-white"
+            >
+              <h2 className="text-xl font-bold mb-4">School Curriculum Support Classes</h2>
+              <p className="text-sm">
+              Excel in your studies with our School Curriculum Support Classes, designed to complement your syllabus with advanced computer lab facilities and hands-on learning.
+              </p>
+            </div>
+            <div
+              style={{ backgroundColor: "#c76dad" }}
+              className="h-64 bg-gray-300 p-6 text-white"
+            >
+              <h2 className="text-xl font-bold mb-4">Financial Aid and Training Programs</h2>
+              <p className="text-sm">
+              Explore our Financial Aid and Training Programs, combining financial support with top-notch training facilities and expert staff to help you succeed.
+              </p>
             </div>
           </div>
         </div>
