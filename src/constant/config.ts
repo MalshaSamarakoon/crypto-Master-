c9ffe81e56f0d1eb943658dfdd1fc8ccc81ac49f
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 22:22:52 +0530

    Update config.ts

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,11 +1,1 @@
-interface SiteConfig {
-  title: string;
-  description: string;
-  url: string;
-}
 
-export const siteConfig: SiteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description: 'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  url: 'https://tsnext-tw.thcl.dev',
-};
