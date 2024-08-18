c42702889381a7aa77cf135c7ab66ea124cacd7c
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:37:25 +0530

    Update mission.tsx

--- src/app/pages/about/mission.tsx
+++ src/app/pages/about/mission.tsx
@@ -4,12 +4,12 @@ import Accent from '@/app/components/Accent';
 export default function Mission() {
   return (
 <div className='mt-8 h-full w-full md:mt-0' dir="rtl">
-  <h2 className='text-4xl md:text-6xl text-right'>
-    <Accent className='inline decoration-clone leading-snug dark:leading-none'>
-      Mission
-    </Accent>
-  </h2>
-  <div className='mt-4 text-base text-gray-100 md:text-lg sm:text-sm text-right'>
+<div className="h-full w-full layout lg:px-0 pt-0">
+<h2 style={{ color: "#01162d" }} className="text-2xl md:text-3xl sm:mx-0 lg:mx-0 mb-10">
+Mission
+</h2>
+</div>
+  <div className='mt-4 text-base text-black-100 md:texmdt-lg text-right'>
   To lead the way in transforming lives through cutting-edge knowledge,
     fostering prosperity, and opening doors to new technological and industrial
     opportunities that empower individuals to achieve financial well-being
