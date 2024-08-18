008ef429c019d7a0d8e28130d9496bbdab1ed2b5
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:36:52 +0530

    Update page.tsx

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import * as React from "react";
-import Head from "next/head";
+import useLoaded from "@/hooks/useLoaded";
 import Vision from "@/app/pages/about/vision";
 import Mission from "@/app/pages/about/mission";
 import TeamList from "@/app/pages/about/TeamList";
@@ -9,48 +9,87 @@ import Seperator from "@/app/components/Seperator";
 export default function AboutPage() {
   return (
     <main>
-            <Head>
-        <title>Moon Education | About</title>
-      </Head>
       <section className=" relative">
-        <div className="lg:mt-20 sm:mt-10 flex min-h-screen flex-col py-1 sm:mx-10 lg:mx-0">
-          <div
-            className="relative content-container z-10 text-lg md:text-xl">
-            <div/> 
-           <div className="layout ">
+        <div
+          className="relative content-container z-10 text-lg md:text-xl flex items-center justify-center"
+          style={{ position: "relative", width: "100%" }}
+        >
+          <img
+            src="/images/yy.png"
+            alt="Hero Image"
+            className="w-full h-full object-cover col-span-2"
+          />
+        </div>
+        <div className="lg:mt-20 sm:mt-10 flex min-h-screen flex-col py-1">
+          <div className="relative content-container z-10 text-lg md:text-xl">
+            <div className="layout">
               <h1
-                className="text-white md:text-5xl md:leading-snug"
+                id="moon-education"
+                style={{ color: "#01162d" }}
+                className="text-black md:text-4xl md:leading-snug mb-8"
                 data-fade="2"
               >
-                Who we are ?
+                Moon Education Center{" "}
               </h1>
-              <p
-                className={clsx(
-                  "my-4 max-w-4xl text-left text-neutral-400 md:my-10",
-                  "md:text-lg sm:text-sm 2xl:text-lg"
-                )}
-                data-fade="3"
+              <div
+                className="text-base text-neutral-800 md:text-m sm:mx-10 lg:mx-0"
+                style={{ lineHeight: "2", letterSpacing: "0px" }}
               >
-                We are a passionate team of educators and experts dedicated to
-                guiding individuals on their journey to financial success.
-                Committed to democratizing knowledge, we provide accessible
-                resources and training, ensuring that everyone has the
-                opportunity to harness the power of new technologies and
-                industries to build wealth and achieve their dreams. Dedicated
-                to democratizing knowledge, we empower financial success through
-                accessible resources and practical guidance.
-              </p>
+                <p className="mb-2 text-justify">
+                  Moon Education Center stands at the forefront of educational
+                  excellence, driven by a commitment to innovation and
+                  comprehensive support. Our mission is to empower individuals
+                  with the knowledge and skills necessary to thrive in today’s
+                  rapidly changing world.
+                </p>
+
+                <p className="mb-2 text-justify">
+                  We offer a diverse range of programs tailored to meet the
+                  unique needs of our students. Our Vocational Training Programs
+                  are designed to provide practical, career-oriented skills,
+                  while our School Curriculum Support Classes help students
+                  achieve academic excellence. Recognizing the importance of
+                  holistic development, Moon Education Center also offers a
+                  range of support services. These include professional
+                  counseling to assist students in navigating their educational
+                  journeys and business advisory services to prepare them for
+                  success in the competitive job market.
+                </p>
+
+                <div className="mt-10 bg-gray-100 p-6">
+                  <Vision />
+                </div>
+                <div className="mt-20 bg-gray-100 p-6">
+                  <Mission />
+                </div>
+
+                <h6 className="text-base text-[#002147] text-lg my-6 font-bold mt-20">
+                  Academic Experience
+                </h6>
+                <p className="mb-2 text-justify">
+                  To enhance the academic experience and create a comfortable,
+                  study-friendly environment, we uphold a high standard of
+                  responsibility and discipline. Our dedicated team of educators
+                  is passionate about fostering student success, employing
+                  innovative teaching methods, and emphasizing hands-on learning
+                  to help every student reach their full potential.
+                </p>
+                <h6 className="text-base text-[#002147] text-lg my-6 font-bold">
+                  Our Facilities
+                </h6>
+                <p className="mb-2 text-justify">
+                  Our facilities reflect our dedication to providing a superior
+                  educational experience. We feature modern halls equipped with
+                  the latest smart board technology, fostering an interactive
+                  and engaging learning environment. Additionally, our
+                  state-of-the-art computer labs are designed to accommodate
+                  both in-person and online learning, ensuring that students
+                  have access to the most up-to-date tools and resources.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="layout">
-            <div className="mt-20">
-              <Vision />
-            </div>
-            <div className="mt-20">
-              <Mission />
-            </div>
-            <Seperator />
-
+          <div id="team-list" className="layout">
             <TeamList />
             <Seperator />
           </div>
