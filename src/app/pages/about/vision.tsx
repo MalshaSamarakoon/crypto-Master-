180c929e6fdbbdb0f534e152503fb66919aa24da
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:35:58 +0530

    Update vision.tsx

--- src/app/pages/about/vision.tsx
+++ src/app/pages/about/vision.tsx
@@ -1,17 +1,21 @@
+
 import React from 'react';
 import Accent from '@/app/components/Accent';
 
 export default function Vision() {
   return (
-    <div className='mt-8 h-full w-full md:mt-0'>
-      <h2 className='text-4xl md:text-6xl'>
-        <Accent className='inline decoration-clone leading-snug dark:leading-none'>
-          Vision
-        </Accent>
-      </h2>
-      <div className='mt-4 text-base text-gray-100 md:text-lg sm:text-sm'>
-        &quot;To lead the way in transforming lives through cutting-edge knowledge, fostering prosperity, and opening doors to new technological and industrial opportunities that empower individuals to achieve financial well-being.&quot;
-      </div>
-    </div>
+<div className='mt-8 h-full w-full md:mt-0'>
+<div className="h-full w-full layout lg:px-0 pt-0">
+          <h2 style={{ color: "#01162d" }} className="text-2xl md:text-3xl sm:mx-0 lg:mx-0 mb-10">
+            Vision
+          </h2>
+        </div>
+  <div className='mt-4 text-base text-black-100 md:text-md'>
+  " To lead the way in transforming lives through cutting-edge knowledge, fostering prosperity, and opening doors to new technological and industrial opportunities that empower individuals to achieve financial well-being. "        
+
+  </div>
+</div>
   );
 }
+
+
