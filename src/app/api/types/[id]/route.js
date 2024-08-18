82dfa87f868e3353d23b3ac0ba38a4b5f6318d62
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:23:42 +0530

    Add files via upload

---
+++ src/app/api/types/[id]/route.js
@@ -0,0 +1,23 @@
+import connectMongoDB from "../../../lib/mongodb";
+import Type from "../../../models/type";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const { name } = await request.json();
+  await connectMongoDB();
+  await Type.create({ name });
+  return NextResponse.json({ message: "Member Created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const types = await Type.find();
+  return NextResponse.json({ types });
+}
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Type.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Member deleted" }, { status: 200 });
+}
