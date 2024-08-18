72a5aa51bc2722ea15b03c975bc01cdd04ce548b
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-08-18 21:43:57 +0530

    Update ChooseLanding.tsx

--- src/app/components/ChooseLanding.tsx
+++ src/app/components/ChooseLanding.tsx
@@ -4,40 +4,30 @@ import clsx from "clsx";
 export default function ChooseLanding() {
   const services = [
     {
-      name: "1. Lifetime Support",
-      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
-      icon: "/images/badge-check.svg",
-    },
-    {
-      name: "2. Modern Learning Technologies",
+      name: "Modern Learning Technologies",
       desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
       icon: "/images/clipboard-list-check.svg",
     },
     {
-      name: "3. Up-to-date Curriculums ",
-      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
-      icon: "/images/lightbulb-dollar.svg",
-    },
-    {
-      name: "4. Personal Attention",
-      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
-      icon: "/images/coins.svg",
-    },
-    {
-      name: "5. Team-based trading",
-      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
-      icon: "/images/people-poll.svg",
-    },
-    {
-      name: "6. Online / Physical  ",
+      name: "Luxurious Environment",
       desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
       icon: "/images/store-alt.svg",
     },
+    {
+      name: "Online / Physical  ",
+      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
+      icon: "/images/badge-check.svg",
+    },
+    {
+      name: "Online / Physical  ",
+      desc: "We let you make the difference. Our goal is to inspire you with new insights and creative solutions.",
+      icon: "/images/badge-check.svg",
+    },
   ];
 
-  const servicesInRows = chunkArray(services, 3);
+  const servicesInRows = chunkArray(services, 4);
 
-  function chunkArray(array:any, chunkSize:any) {
+  function chunkArray(array: any, chunkSize: any) {
     const result = [];
     for (let i = 0; i < array.length; i += chunkSize) {
       result.push(array.slice(i, i + chunkSize));
@@ -47,38 +37,45 @@ export default function ChooseLanding() {
 
   return (
     <section className="relative">
-    <div className="h-full w-full layout items-center px-20">
-      <h2 className="text-4xl md:text-6xl text-right ">
-        <Accent className="inline decoration-clone leading-snug dark:leading-none">
-          Why Choose Us?
-        </Accent>
-      </h2>
-      <div className=" mt-20">
-
-      {servicesInRows.map((row, rowIndex) => (
-        <div key={rowIndex} className=" grid grid-cols-3 sm:grid-cols-1 md:grid-cols-3 flex justify-between lg:p-10 sm:p-0">
-          {row.map((service:any, index:any) => (
+      <div className="h-full w-full layout items-center px-0">
+        <div className=" mt-0">
+          {servicesInRows.map((row, rowIndex) => (
             <div
-              key={index}
-              className="flex flex-col items-center box-container relative "
+              key={rowIndex}
+              className=" grid grid-cols-4 sm:grid-cols-1 md:grid-cols-4 flex justify-between lg:p-10 sm:p-0"
             >
-              {service.icon && <img src={service.icon} className="icon lg:w-20 lg:h-20 lg:mb-10 sm:w-10 sm:h-10 sm:mb-6" />}
+              {row.map((service: any, index: any) => (
+                <div
+                  key={index}
+                  className="flex flex-col items-center box-container relative "
+                >
+                  {service.icon && (
+                    <img
+                      src={service.icon}
+                      className="icon lg:w-20 lg:h-20 lg:mb-10 sm:w-10 sm:h-10 sm:mb-6"
+                    />
+                  )}
 
-              <div
-                className="text-base text-amber-300 md:text-xl text-center "
-                style={{ lineHeight: "2", letterSpacing: "0.9px" }}
-              >
-                {service.name}
-              </div>
-              <div className="text-base text-neutral-400 md:text-md text-center lg:mb-10 sm:mb-10">
-                {service.desc}
-              </div>
+                  <div
+                    className="text-base md:text-xl text-center"
+                    style={{
+                      color: "#8d6e1b",
+                      lineHeight: "2",
+                      letterSpacing: "0.9px",
+                    }}
+                  >
+                    {service.name}
+                  </div>
+
+                  <div className="text-base text-neutral-400 md:text-sm text-center lg:mb-10 sm:mb-10">
+                    {service.desc}
+                  </div>
+                </div>
+              ))}
             </div>
           ))}
-        </div>
-      ))}
-    </div>    </div>
-
-  </section>
+        </div>{" "}
+      </div>
+    </section>
   );
 }
