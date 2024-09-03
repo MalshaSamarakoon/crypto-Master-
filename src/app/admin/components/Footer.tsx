0f41743572b9698306778043b309bd5ed4b4c618
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-09-03 19:44:04 +0530

    Update Footer.tsx

--- src/app/admin/components/Footer.tsx
+++ src/app/admin/components/Footer.tsx
@@ -4,6 +4,15 @@ import * as React from 'react';
 
 export default function AdFooter() {
   return (
-   
+    <footer className='z-50 relative'>
+      <div className='bg-neutral-900 p-6 text-center dark:bg-neutral-100'>
+        <a
+          className='font-semibold text-neutral-600 dark:text-neutral-400'
+          href={`${process.env.BASE_URL}/`}
+        >
+          Moon Education
+        </a>
+      </div>
+    </footer>
   );
 }
