591c6e5f9dd37b80c414a8939d71720f602550b1
Malsha Samarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2024-09-08 18:25:14 +0530

    Update PraiseLanding.tsx

--- src/app/components/PraiseLanding.tsx
+++ src/app/components/PraiseLanding.tsx
@@ -8,25 +8,19 @@ export default function Praise() {
         className="flex items-center justify-center md:py-2 sm:py-1 border-y border-neutral-500 text-neutral-400"
       >
         <div className="flex w-full">
-          <div className="w-1/4 px-2">
+          <div className="w-1/3 px-2">
             <div className="flex flex-col items-center md:flex-row md:items-center md:justify-center border-r border-neutral-500">
               <h2 className="text-xs md:text-3xl mr-2">24/7</h2>
-              <h5 className="text-xs md:text-lg">Service</h5>
+              <h5 className="text-xs md:text-lg ">Service</h5>
               </div>
           </div>
-          <div className="w-1/4 px-2">
+          <div className="w-1/3 md:px-2">
             <div className="flex flex-col items-center md:flex-row md:items-center md:justify-center border-r border-neutral-500">
               <h2 className="text-xs md:text-3xl mr-2">20+</h2>
               <h5 className="text-xs md:text-lg"> Lecturers</h5>
             </div>
           </div>
-          <div className="w-1/4 md:px-2">
-            <div className="flex flex-col items-center md:flex-row md:items-center md:justify-center border-r border-neutral-500">
-              <h2 className="text-xs md:text-3xl mr-2">3+ </h2>
-              <h5 className="text-xs md:text-lg"> Years Experience</h5>
-            </div>
-          </div>
-          <div className="w-1/4 px-2">
+          <div className="w-1/3 px-2">
             <div className="flex flex-col items-center md:flex-row md:items-center md:justify-center">
               <h2 className="text-xs md:text-3xl mr-2">1000+</h2>
               <h5 className="text-xs md:text-lg">Students</h5>
